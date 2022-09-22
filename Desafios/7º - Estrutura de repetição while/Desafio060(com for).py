c86da46e952df4d2f6e089d2e6be496bbebb6a2c
Diego Frollini <diegopicello@hotmail.com>
2022-09-22 14:16:31 -0300

    Update Desafio060(com for).py

--- Desafios/7º - Estrutura de repetição while/Desafio060(com for).py
+++ Desafios/7º - Estrutura de repetição while/Desafio060(com for).py
@@ -1,19 +1,12 @@
-fat = 1
-n = int(input('Digite um número inteiro para calcular seu fatorial: '))
-c = n
-if n == 1:
-    print('1 x 1 = 1')
-elif n == 0:
-    print('n! = n . (n - 1)! -> considerando n = 1:')
-    print('1! = 1 . (1 - 1)!')
-    print('1! = 1 . 0!')
-    print('1 = 0!')
-else:
-    for c in range(c, 0, -1):
-        fat = fat * n
-        if n != 1:
-            print('{} x '.format(n), end = '')
-        else:
-            print('{} = {}'.format(n, fat))
-        n -= 1
-print('O resultado do fatorial é {}.'.format(fat))
+n = int(input('Digite um número inteiro não negativo: '))
+f = 1
+print('{}! = '.format(n), end = '')
+for c in range(n, 0, -1):
+    print('{}'.format(n), end = '')
+    if n > 1:
+        print(' x ', end = '')
+    else:
+        print(' = ', end = '')
+    f *= n
+    n -= 1
+print(f)
