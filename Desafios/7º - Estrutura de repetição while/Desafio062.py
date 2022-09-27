31e168b1792872df525ae881bda0196cb655ad0f
Diego Frollini <diegopicello@hotmail.com>
2022-09-27 10:12:07 -0300

    Update Desafio062.py

--- Desafios/7º - Estrutura de repetição while/Desafio062.py
+++ Desafios/7º - Estrutura de repetição while/Desafio062.py
@@ -1,4 +1,5 @@
 n = 1
+total = 10
 print('=-' * 10)
 print('Gerador de PA')
 print('=-' * 10)
@@ -13,8 +14,9 @@ for c in range(10, -1, -1):
         an = an + r     
 while n != 0:
     n = int(input('Quantos termos subsequentes você ainda quer mostrar? '))
+    total += n
     if n == 0:
-        print('Fim da execução.')
+        print('Fim da execução com {} termos mostrados.'.format(total))
     else:
         for c in range(n, -1, -1):
             if c != 0:
