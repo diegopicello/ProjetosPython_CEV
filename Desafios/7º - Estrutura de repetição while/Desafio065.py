dd34e7a6c92a2773a02865faaa37bf09961291b5
Diego Frollini <diegopicello@hotmail.com>
2022-10-02 20:07:16 -0300

    Update Desafio065.py

--- Desafios/7º - Estrutura de repetição while/Desafio065.py
+++ Desafios/7º - Estrutura de repetição while/Desafio065.py
@@ -1,10 +1,11 @@
+resp = 'sim'
 soma = cont = 0
 primeiro = float(input('Digite um número: '))
 soma += primeiro
 menor = maior = primeiro
 cont = 1
-sn = str(input('Quer continuar?[s/n] '))
-while sn not in 'Nn':
+resp = str(input('Quer continuar?[s/n] ')).lower().strip()[0]
+while resp not in 'Nn':
     num = float(input('Digite um número: '))
     soma += num
     if num < menor:
@@ -12,7 +13,7 @@ while sn not in 'Nn':
     if num > maior:
         maior = num
     cont += 1
-    sn = str(input('Quer continuar?[s/n] '))
+    resp = str(input('Quer continuar?[s/n] ')).lower().strip()[0]
 media = soma / cont
 print('Você digitou {} números e a média foi de {}.'.format(cont, media))
 print('O maior valor foi {} e o menor valor foi {}.'.format(maior, menor))
