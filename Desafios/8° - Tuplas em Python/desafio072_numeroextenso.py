25ac4750721a3bf973ddc0dca1d5c9e944a3ec55
Diego Frollini <diegopicello@hotmail.com>
2022-10-18 21:04:02 -0300

    desafio 072

---
+++ Desafios/8° - Tuplas em Python/desafio072_numeroextenso.py
@@ -0,0 +1,15 @@
+"""Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. 
+Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso."""
+extenso = ('zero', 'um', 'dois', 'três', 'quatro',
+        'cinco', 'seis', 'sete', 'oito', 'nove',
+        'dez','onze', 'doze', 'treze', 'catorze',
+        'quinze', 'dezesseis', 'dezessete', 'dezoito',
+        'dezenove', 'vinte')
+while True:
+    num = int(input('Digite um número entre 0 e 20: '))
+    if num >= 0 and num <= 20:
+        break
+    print('Tente novamente!', end = ' ')
+print(num)
+print(f'Você digitou o número {extenso[num]}.')
+
--- Teoria e testes/Aula16/Teste02.py
+++ Teoria e testes/Aula16/Teste02.py
@@ -7,4 +7,4 @@ c = a + b
 print(c)#a ordem aqui importa: a + b fica 2, 5, 4, 5, 8, 1, 2, já b + a ficaria 5,8,1,2,2,5,4
 print(c.count(5)) #conta quantas vezes 5 aparece na tupla c
 print(c.index(8)) #mostra a posição do número que pdimos dentro da tupla. Esse método sempre pega a primeira ocorrência EX: na tupla C, só mostraria a primeira ocorrencia de 2
-print(c.index(5, 2))
+print(c.index(5, 2))#Esse número após o 5 é a partir de onde voce quer comecar a contar seu index: Nesse caso, começo a procurar um número 5 apenas após a posição 2!
