f00dd9b509bfaa8f9aaf61a2e52f31aca64c0ed9
Diego Frollini <diegopicello@hotmail.com>
2024-07-13 16:24:50 -0300

    desafio097

--- Desafios/11° - Dicionários em Python/desafio096_carttrab.py
+++ Desafios/11° - Dicionários em Python/desafio096_carttrab.py
@@ -1,10 +1,11 @@
 '''Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-o (com idade) em um dicionário. 
 Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário. 
 Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.'''
+from datetime import datetime
 dados = dict()
 dados['Nome'] = str(input('Digite o nome: '))
 dados['ano nasc'] = int(input('Digite o ano de nascimento: '))
-idade = 2024 - dados['ano nasc']
+idade = datetime.now().year - dados['ano nasc']
 dados['Nº carteira'] = int(input('Digite o número da Carteira de Trabalho (digite "0" caso não tenha): '))
 if dados['Nº carteira'] != 0:
     dados['Ano de contratação'] = int(input('Digite o ano de contratação: '))
