d54722eb71505c8150add422c7b64265f6803af9
Diego Frollini <diegopicello@hotmail.com>
2024-09-09 22:55:05 -0300

    Create desafio108_validando.py

---
+++ Desafios/13° - Funções em Python (parte 2)/desafio108_validando.py
@@ -0,0 +1,14 @@
+'''Crie um programa que tenha a função leiaInt(), 
+que vai funcionar de forma semelhante ‘a função input() do Python, 
+só que fazendo a validação para aceitar apenas um valor numérico. Ex: n = leiaInt(‘Digite um n: ‘)'''
+def Leiaint(texto):
+    while True:
+        n = str(input(texto)).strip()
+        if n.isnumeric() is False:
+            print('ERRO! Digite um número válido.')
+        else:
+            return n
+
+#programa principal:
+n = Leiaint('Digite um número: ')
+print(f'Você digitou o valor {n}')
