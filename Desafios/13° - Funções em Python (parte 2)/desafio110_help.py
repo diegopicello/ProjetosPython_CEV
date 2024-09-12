d38a23b05d5bea0ad8c85e40042a8664379d531b
Diego Frollini <diegopicello@hotmail.com>
2024-09-12 17:29:01 -0300

    Create desafio110_help.py

---
+++ Desafios/13° - Funções em Python (parte 2)/desafio110_help.py
@@ -0,0 +1,26 @@
+'''Faça um mini-sistema que utilize o Interactive Help do Python. 
+O usuário vai digitar o comando e o manual vai aparecer. Quando o usuário digitar a palavra FIM, 
+o programa se encerrará. Importante: use cores.
+'''
+from time import sleep
+def ajuda(texto):
+    print('='*(30 + len(texto)))
+    print(f' Acessando manual do comando {texto}')
+    print('='*(30 + len(texto)))
+    for c in range(0,3):
+        print('. ', end='', flush=True)
+        c += 1
+        sleep(0.5)
+    print(help(texto))
+
+
+#programa principal:
+func = ''
+print('~'*25)
+print(' Sistema de ajuda PYhelp')
+print('~'*25)
+while True:
+    func = str(input('Digite a função ou biblioteca desejada: ')).strip()
+    ajuda(func)
+    if func == 'FIM':
+        break
