8568030312105b1195a6ac584fc075365f8133f5
Diego Frollini <diegopicello@hotmail.com>
2024-09-20 13:31:22 -0300

    updatedesafio113

--- Desafios/14° - Modularização/moedas.py
+++ Desafios/14° - Modularização/moedas.py
@@ -1,30 +1,48 @@
 '''Funções para cálculos básicos, como aumento, dobro, etc.'''
-def aumentar(valor, tanto):
+def moeda(valor, cifrão = 'R$'):
+    '''---> formata um valor float qualquer para um valor monetário;
+    parâmetro valor: o valor a ser formatado;
+    parâmetro cifrão: a moeda nacional a ser utilizada;
+    retorno: o valor formatado.'''
+    return f'{cifrão}{valor:.2f}'.replace('.',',')
+
+def aumentar(valor, tanto, formatação = True):
     '''---> Calcula o aumento do valor;
     parâmetro valor: o input que será aumentado;
     parâmetro tanto: a magnitude do aumento;
     retorno: o valor aumentado em (1 + tanto)%.'''
     valor *= (1 + tanto)
-    return valor
+    if formatação:
+        novovalor = moeda(valor)
+    return novovalor
 
-def diminuir(valor, tanto):
+def diminuir(valor, tanto, formatação = True):
     '''---> Calcula a diminução do valor;
     parâmetro valor: o input que será diminuído;
     parâmetro tanto: a magnitude da diminuição;
     retorno: o valor diminuído em tanto% dele.'''
     valor *= tanto
-    return valor
+    if formatação:
+        novovalor = moeda(valor)
+    return novovalor
 
-def dobro(valor):
-    '''---> Calcula a metade do parâmetro valor.'''
+def dobro(valor, formatação = True):
+    '''---> Calcula a metade do parâmetro valor;
+    parâmetro valor: o valor utilizado para o cálculo;
+    parâmetro formatação: apresentar valor com formatação monetária;
+    retorno: o dobro do valor com formatação monetária.'''
     valor *= 2
-    return valor
+    if formatação:
+        novovalor = moeda(valor)
+    return novovalor
 
-def metade(valor):
-    '''---> Calcula a metade do  parâmetro valor.'''
+def metade(valor, formatação = True):
+    '''---> Calcula a metade do  parâmetro valor;
+    parâmetro valor: o valor utilizado para o cálculo;
+    parâmetro formatação: apresentar valor com formatação monetária;
+    retorno: a metade do valor com formatação monetária.'''
     valor *= 0.5
-    return valor
-
-def moeda(valor):
-    return f'R${valor:.2f}'.replace('.',',')
-print(moeda(200))
+    if formatação:
+        novovalor = moeda(valor)
+    return novovalor
+print(dobro(200))
