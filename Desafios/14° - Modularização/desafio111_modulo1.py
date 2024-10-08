93c79e285638d3f5240287d8a21bcac0e056e31e
Diego Frollini <diegopicello@hotmail.com>
2024-10-08 19:39:19 -0300

    pacote

--- Desafios/14° - Modularização/desafio111_modulo1.py
+++ Desafios/14° - Modularização/desafio111_modulo1.py
@@ -1,6 +1,6 @@
 '''Crie um módulo chamado moeda.py que tenha as 
 funções incorporadas aumentar(), diminuir(), dobro() e metade(). 
 Faça também um programa que importe esse módulo e use algumas dessas funções.'''
-import moedas
+from utilidadesCeV import moedas
 preço = float(input('Digite um preço: '))
 moedas.resumo(preço, 10, 20)
