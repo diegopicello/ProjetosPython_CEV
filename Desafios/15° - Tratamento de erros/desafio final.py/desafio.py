9859133db5c8eac019718cf2c1296f844cdaa9f7
Diego Frollini <diegopicello@hotmail.com>
2025-01-11 09:52:54 -0300

    .

--- Desafios/15° - Tratamento de erros/desafio final.py/desafio.py
+++
@@ -1,6 +0,0 @@
-'''Criar um menu em Python usando modularização.'''
-import modulo
-
-#modulo.cabecalho('Menu principal')
-modulo.opções('Ver cadastros', 'Cadastrar nova pessoa', 'Sair do sistema')
- 
