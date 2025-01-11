6c1168a239a7fff81054ce4682a59fb3504c45ac
Diego Frollini <diegopicello@hotmail.com>
2025-01-11 09:42:47 -0300

    .

---
+++ Desafios/15° - Tratamento de erros/desafio final.py/desafio.py
@@ -0,0 +1,6 @@
+'''Criar um menu em Python usando modularização.'''
+import modulo
+
+modulo.cabeçalho('Menu Principal')
+modulo.opções('Ver cadastros', 'Cadastrar nova pessoa', 'Sair do sistema')
+ 
---
+++ Desafios/15° - Tratamento de erros/desafio113_site.py
@@ -0,0 +1,11 @@
+'''Crie um código em Python que teste se o site pudim está acessível pelo computador usado.'''
+import urllib
+import urllib.request
+
+try:
+    site = urllib.request.urlopen('http://www.youtube.com.br')
+except:
+    print('Site não está disponível no momento!')
+else:
+    print('Site está funcionando normalmente.')
+    print(site.read)
