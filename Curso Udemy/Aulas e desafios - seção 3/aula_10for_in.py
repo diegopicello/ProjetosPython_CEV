ae1764f991fe1dd5bdaf88dbad3618d58182f80f
Diego Frollini <diegopicello@hotmail.com>
2025-02-20 11:46:51 -0300

    .

---
+++ Curso Udemy/Aulas e desafios - seção 3/aula_10for_in.py
@@ -0,0 +1,4 @@
+texto = 'Ribeirão Preto'
+#sei que é iterável: o for só precisa de um iterável e ele vai iterar;
+for letra in texto:
+    print(letra)
--- Curso Udemy/Aulas e desafios - seção 3/exercicio70_contarletras.py
+++ Curso Udemy/Aulas e desafios - seção 3/exercicio70_contarletras.py
@@ -1,3 +1,14 @@
 frase = 'O Python é uma linguagem de programação '\
     'multiparadigma. '\
-    'Python foi criado por Guido van Rossum.'
+    'Python foi criado por Guido van Rossum.'.lower()
+i = 0
+maisvezes = 0
+letramaisvezes = ''
+while i < len(frase):
+    letraatual = frase[i]
+    vezesapareceu = frase.count(letraatual)
+    if vezesapareceu > maisvezes and letraatual != ' ':
+        letramaisvezes = letraatual
+        maisvezes = vezesapareceu
+    i += 1
+print(f'A letra que apareceu mais vezes foi "{letramaisvezes}" com {maisvezes} aparições na frase.')
