8f473e6f516c1078fd3dd278a0e17f71ca3d8d69
Diego Frollini <diegopicello@hotmail.com>
2025-02-20 11:48:16 -0300

    Update aula_10for_in.py

--- Curso Udemy/Aulas e desafios - seção 3/aula_10for_in.py
+++ Curso Udemy/Aulas e desafios - seção 3/aula_10for_in.py
@@ -1,4 +1,7 @@
 texto = 'Ribeirão Preto'
+novotexto = ''
 #sei que é iterável: o for só precisa de um iterável e ele vai iterar;
 for letra in texto:
     print(letra)
+    novotexto += f'*{letra}'
+print(novotexto + '*')
