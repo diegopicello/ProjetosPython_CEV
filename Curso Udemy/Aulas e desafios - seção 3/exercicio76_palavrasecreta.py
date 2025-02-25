d1c19f323136367a40f351f6287d2a044149b0e6
Diego Frollini <diegopicello@hotmail.com>
2025-02-25 14:45:45 -0300

    Update exercicio76_palavrasecreta.py

---
+++ Curso Udemy/Aulas e desafios - seção 3/exercicio76_palavrasecreta.py
@@ -0,0 +1,42 @@
+'''"""
+Faça um jogo para o usuário adivinhar qual
+a palavra secreta.
+- Você vai propor uma palavra secreta
+qualquer e vai dar a possibilidade para
+o usuário digitar apenas uma letra.
+- Quando o usuário digitar uma letra, você 
+vai conferir se a letra digitada está
+na palavra secreta.
+    - Se a letra digitada estiver na
+    palavra secreta; exiba a letra;
+    - Se a letra digitada não estiver
+    na palavra secreta; exiba *.
+Faça a contagem de tentativas do seu
+usuário.'''
+
+PALAVRA_SECRETA = 'perfume'
+letra_usuario = ''
+palavra_formada = f'{len(PALAVRA_SECRETA)*'*'}'
+
+while True:
+    letra_usuario = input('Digite uma letra: ').strip().lower()
+    if len(letra_usuario) > 1:
+        print('Por favor, digite apenas uma letra.')
+        continue
+    novastr = ''
+    for letra in PALAVRA_SECRETA:
+        if letra == letra_usuario:
+            novastr += letra_usuario
+        else:
+            novastr += '*'
+    palavrafinal = ''
+    for i, caractere in enumerate(palavra_formada):
+        if PALAVRA_SECRETA[i] == novastr[i] or palavra_formada[i] != '*':
+            palavrafinal += PALAVRA_SECRETA[i]
+        else:
+            palavrafinal += '*'
+    palavra_formada = palavrafinal
+    print(palavra_formada)
+    if palavra_formada == PALAVRA_SECRETA:
+        print('Parabéns, você acertou a palavra.')
+        break
